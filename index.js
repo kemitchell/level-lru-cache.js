a58c8c4089c3a3107bd73e50d7c16d9f5c226eac
Kyle Mitchell <kyle@kemitchell.com>
2015-09-05 14:38:43 -0700

    Add notes to readers

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ var noError = null
 
 module.exports = LevelLRUCache
 
+// Notes to readers:
+//
+// Code and comments call keys that users use to `set` and `get` cache values
+// "cache keys".They call keys in the underlying LevelUP store "Level keys".
+//
+// The cache uses bytewise for partitioning the key spaces of its underlying
+// LevelUP. In bytewise' scheme, `null` is the lowest possible value and
+// `undefined` is the highest. The range from `[ 'key', null ]` to `[ 'key',
+// undefined ]` is any `Array` whose first element is the string `'key'`.
+
 function LevelLRUCache(level, limit) {
   this.level = level
   this.limit = ( limit === undefined ? undefined : ( limit - 1 ) ) }
