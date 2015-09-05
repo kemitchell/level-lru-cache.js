21d9d9ad94e71cd68ac8a6a01d380d50c4c089b7
Kyle Mitchell <kyle@kemitchell.com>
2015-09-05 15:38:50 -0700

    Avoid extra key scans

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var asap = require('asap')
 var bytewise = require('bytewise')
 
 var encode = bytewise.encode
@@ -10,7 +9,7 @@ module.exports = LevelLRUCache
 // Notes to readers:
 //
 // Code and comments call keys that users use to `set` and `get` cache values
-// "cache keys". They call keys in the underlying LevelUP store "Level keys".
+// "cache keys". They call keys in the underlying LevelUP store "level keys".
 //
 // The cache uses bytewise for partitioning the key spaces of its underlying
 // LevelUP. In bytewise' scheme, `null` is the lowest possible value and
@@ -21,7 +20,6 @@ function LevelLRUCache(level, limit) {
   this.level = level
   this.limit = ( limit === undefined ? undefined : ( limit - 1 ) ) }
 
-
 function deleteOperation(levelKey) {
   return { type: 'del', key: levelKey } }
 
@@ -31,12 +29,22 @@ function putOperation(cacheKey, value) {
     key: bytewise.encode([ cacheKey, Date.now() ]),
     value: value } }
 
-LevelLRUCache.prototype._createAllKeysReadStream = function() {
-  return this.level.createReadStream({
+// Calls back with an array of all level keys in the underlying LevelUP.
+LevelLRUCache.prototype._allLevelKeys = function(callback) {
+  var levelKeys = [ ]
+  var errored = false
+  this.level.createReadStream({
     keys: true,
     values: false,
     gt: encode([ null ]),
-    lt: encode([ undefined ]) }) }
+    lt: encode([ undefined ]) })
+  .on('data', function(key) {
+    levelKeys.push(key) })
+  .on('error', function(error) {
+    errored = true
+    callback(error) })
+  .on('end', function() {
+    if (!errored) { callback(noError, levelKeys) } }) }
 
 // Cache a value by key.
 LevelLRUCache.prototype.put = function(cacheKey, value, callback) {
@@ -49,7 +57,7 @@ LevelLRUCache.prototype.put = function(cacheKey, value, callback) {
   cache._trimOperations(function(error, trimOperations) {
     if (error) { callback(error) }
     else {
-      cache._existingLevelKeys(cacheKey, function(error, existingLevelKeys) {
+      cache._existingLevelKeysForCacheKey(cacheKey, function(error, existingLevelKeys) {
         if (error) { callback(error) }
         else {
           var batchOperations = trimOperations
@@ -64,64 +72,60 @@ LevelLRUCache.prototype.put = function(cacheKey, value, callback) {
 
 // Call back with the number of cache records that should be deleted to
 // stay within the cache limit.
-LevelLRUCache.prototype._extra = function(callback) {
+LevelLRUCache.prototype._extra = function(count) {
   var limit = this.limit
   // If there isn't a limit, don't delete anything.
-  if (limit === undefined) {
-    asap(function() { callback(noError, 0) }) }
+  if (limit === undefined) { return 0 }
   else {
-    // Get the current cache record count ...
-    this.count(function(error, count) {
-      if (error) { callback(error) }
-      else {
-        // ... and call back with how it exceeds the cache limit.
-        if (count > limit) {
-          callback(noError, ( count - limit )) }
-        else {
-          callback(noError, 0) } } }) } }
+    if (count > limit) { return ( count - limit ) }
+    else { return 0 } } }
 
 // Call back with all the LevelUP keys for a given cache key.
-LevelLRUCache.prototype._existingLevelKeys = function(cacheKey, callback) {
+LevelLRUCache.prototype._existingLevelKeysForCacheKey = function(cacheKey, callback) {
   var levelKeys = [ ]
+  var errored = false
   this.level.createReadStream({
     keys: true,
     values: false,
     gt: encode([ cacheKey, null ]),
     lt: encode([ cacheKey, undefined ]) })
+  .on('error', function(error) {
+    errored = true
+    callback(error) })
   .on('data', function(levelKey) {
     levelKeys.push(levelKey) })
   .on('end', function() {
-    callback(noError, levelKeys) }) }
+    if (!errored) { callback(noError, levelKeys) } }) }
 
 // Call back with a list of LevelUP batch operations from clearing old
 // records that need to be trimmed to say within the cache's limit.
 LevelLRUCache.prototype._trimOperations = function(callback) {
   var cache = this
-  this._extra(function(error, extra) {
-    // We're still within quota. No need to trim anything.
-    if (extra < 1) {
-      asap(function() { callback(noError, [ ]) }) }
+  this._allLevelKeys(function(error, levelKeys) {
+    if (error) { callback(error) }
     else {
-      // A map from cache key to Array of LevelUP keys
-      var cacheKeyToLevelKeysMap = { }
-      // A map from cache key to latest timestamp
-      var cacheKeyToTimestampMap = { }
-      cache._createAllKeysReadStream()
-      .on('data', function(levelKey) {
-        var decoded = decode(levelKey)
-        var cacheKey = decoded[0]
-        var timestamp = decoded[1]
-        // Note the cache key has a record with the timestamp.
-        if (!cacheKeyToLevelKeysMap.hasOwnProperty(cacheKey)) {
-          cacheKeyToLevelKeysMap[cacheKey] = [ ] }
-        cacheKeyToLevelKeysMap[cacheKey].push(levelKey)
-        // Note this timestamp if it's the newest we've seen for its cache key.
-        var newer = (
-          !cacheKeyToTimestampMap.hasOwnProperty(cacheKey) ||
-          timestamp > cacheKeyToTimestampMap[cacheKey])
-        if (newer) {
-          cacheKeyToTimestampMap[cacheKey] = timestamp } })
-      .on('end', function() {
+      var cacheKeyCount = countCacheKeys(levelKeys)
+      var extra = cache._extra(cacheKeyCount)
+      // We're still within quota. No need to trim anything.
+      if (extra < 1) { callback(noError, [ ]) }
+      else {
+        // A map from cache key to Array of LevelUP keys
+        var cacheKeyToLevelKeysMap = { }
+        // A map from cache key to latest timestamp
+        var cacheKeyToTimestampMap = { }
+        levelKeys.forEach(function(levelKey) {
+          var decoded = decode(levelKey)
+          var cacheKey = decoded[0]
+          var timestamp = decoded[1]
+          // Note the cache key has a record with the timestamp.
+          if (!cacheKeyToLevelKeysMap.hasOwnProperty(cacheKey)) {
+            cacheKeyToLevelKeysMap[cacheKey] = [ ] }
+          cacheKeyToLevelKeysMap[cacheKey].push(levelKey)
+          // Note this timestamp if it's the newest we've seen for its cache key.
+          var newest = (
+            !cacheKeyToTimestampMap.hasOwnProperty(cacheKey) ||
+            timestamp > cacheKeyToTimestampMap[cacheKey])
+          if (newest) { cacheKeyToTimestampMap[cacheKey] = timestamp } })
         // Create a sorted Array of [ cache key, latest timestamp ]
         var keyLatestPairs = Object.keys(cacheKeyToTimestampMap)
         .reduce(
@@ -133,8 +137,7 @@ LevelLRUCache.prototype._trimOperations = function(callback) {
         // [ cache key, latest timestamp ] elements for the records that need
         // to be trimmed are at the front of that Array.
         var toDelete = keyLatestPairs.slice(0, extra)
-        .map(function(element) {
-          return element[0] })
+        .map(function(element) { return element[0] })
         // Create an Array of LevelUP batch operations to delete ...
         var batchOperations = toDelete
           .reduce(
@@ -144,22 +147,27 @@ LevelLRUCache.prototype._trimOperations = function(callback) {
               return batch.concat(levelKeys.map(deleteOperation)) },
             [ ])
         // ... and call back with it.
-        callback(noError, batchOperations) }) } }) }
+        callback(noError, batchOperations) } } }) }
+
+function countCacheKeys(levelKeys) {
+  // Scan all level keys, counting cache keys.
+  var cacheKeysSeen = [ ]
+  return levelKeys
+    .reduce(
+      function(count, levelKey) {
+        var decoded = decode(levelKey)
+        var cacheKey = decoded[0]
+        // Don't count a cache key twice if seen before.
+        return (
+          ( cacheKeysSeen.indexOf(cacheKey) === -1 ) ?
+            ( count + 1 ) : count ) },
+      0) }
 
 // Call back with the number of cache keys in the underlying LevelUP.
 LevelLRUCache.prototype.count = function(callback) {
-  // Scan call cache keys, counting keys.
-  var count = 0
-  var cacheKeysSeen = [ ]
-  this._createAllKeysReadStream()
-  .on('data', function(levelKey) {
-    var decoded = decode(levelKey)
-    var cacheKey = decoded[0]
-    // Don't count a key twice if seen before.
-    if (cacheKeysSeen.indexOf(cacheKey) === -1) {
-      count += 1 } })
-  .on('end', function() {
-    callback(noError, count) }) }
+  this._allLevelKeys(function(error, levelKeys) {
+    if (error) { callback(error) }
+    else { callback(noError, countCacheKeys(levelKeys)) } }) }
 
 // Call back with the cached value for a cache key.
 LevelLRUCache.prototype.get = function(cacheKey, callback) {
@@ -167,10 +175,9 @@ LevelLRUCache.prototype.get = function(cacheKey, callback) {
     throw new TypeError('key must be a string') }
   var cache = this
   // Find all existing LevelUP keys with the key.
-  cache._existingLevelKeys(cacheKey, function(error, existingLevelKeys) {
+  cache._existingLevelKeysForCacheKey(cacheKey, function(error, existingLevelKeys) {
     // If there aren't any, return undefined.
-    if (existingLevelKeys.length === 0) {
-      callback(noError, undefined) }
+    if (existingLevelKeys.length === 0) { callback(noError, undefined) }
     else {
       // Identify the latest timestamp and value for the key.
       var latestTimestamp = 0
