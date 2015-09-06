a5a1c6030ab366713f4988e947421365c8c9a639
Kyle Mitchell <kyle@kemitchell.com>
2015-09-05 17:13:31 -0700

    Note use of hex bytewise in comment

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ module.exports = LevelLRUCache
 // Code and comments call keys that users use to `set` and `get` cache values
 // "cache keys". They call keys in the underlying LevelUP store "LevelUP keys".
 //
-// The cache uses bytewise for partitioning the key spaces of its underlying
-// LevelUP. In bytewise' scheme, `null` is the lowest possible value and
-// `undefined` is the highest. The range from `[ 'key', null ]` to `[ 'key',
-// undefined ]` is any `Array` whose first element is the string `'key'`.
+// The cache uses bytewise (with hex encoding) for partitioning the key spaces
+// of its underlying // LevelUP. In bytewise' scheme, `null` is the lowest
+// possible value and // `undefined` is the highest. The range from `[ 'key',
+// null ]` to `[ 'key', // undefined ]` is any `Array` whose first element is
+// the string `'key'`.
 
-function LevelLRUCache(level, limit) {
-  this.level = level
+function LevelLRUCache(level, limit) { this.level = level
   this.limit = ( limit === undefined ? undefined : ( limit - 1 ) ) }
 
 LevelLRUCache.prototype.count = require('./public/count')
