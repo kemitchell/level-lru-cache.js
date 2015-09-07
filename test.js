7594dc06e39624982b974a6f303a6071a3174466
Kyle Mitchell <kyle@kemitchell.com>
2015-09-06 20:43:04 -0700

    Crib tests from lru-cache

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var Cache = require('./')
 var levelup = require('levelup')
 var memdown = require('memdown')
+var series = require('async-series')
 var tape = require('tape')
 
 function testCache(limit) {
@@ -13,14 +14,14 @@ tape.test('string keys', function(test) {
   test.throws(
     function() { cache.put(new Date(), 'x') },
     /string/,
-    '.set() requires string keys')
+    '.put() requires string keys')
   test.throws(
     function() { cache.get(new Date()) },
     /string/,
     '.get() requires string keys')
   test.end() })
 
-tape.test('set and get', function(test) {
+tape.test('put and get', function(test) {
   test.plan(5)
   var cache = testCache()
   cache.put('a', 'b', function(error) {
@@ -42,3 +43,94 @@ tape.test('rolls off old values', function(test) {
       cache.get('a', function(error, value) {
         test.error(error, 'no get error')
         test.equal(value, undefined, 'first cached is undefined') }) }) }) })
+
+tape.test('missing value', function(test) {
+  test.plan(1)
+  var cache = testCache()
+  cache.get('nada', function(error, value) {
+    test.equal(value, undefined, 'nonexistent value is undefined') }) })
+
+tape.test('least recently put', function (test) {
+  var cache = new testCache(2)
+  series(
+    [ cache.put.bind(cache, 'a', 'A'),
+      cache.put.bind(cache, 'b', 'B'),
+      cache.put.bind(cache, 'c', 'C'),
+      function(done) {
+        cache.get('c', function(error, value) {
+          test.ifError(error, 'no error')
+          test.equal(value, 'C', 'c is C')
+          done() }) },
+      function(done) {
+        cache.get('b', function(error, value) {
+          test.ifError(error, 'no error')
+          test.equal(value, 'B', 'b is B')
+          done() }) },
+      function(done) {
+        cache.get('a', function(error, value) {
+          test.ifError(error, 'no error')
+          test.equal(value, undefined, 'a is undefined')
+          done() }) } ],
+    function(error) {
+      test.ifError(error, 'no error')
+      test.end() }) })
+
+tape.test('recently gotten', function (test) {
+  var cache = new testCache(2)
+  series(
+    [ cache.put.bind(cache, 'a', 'A'),
+      cache.put.bind(cache, 'b', 'B'),
+      function(done) {
+        cache.get('a', function(error, value) {
+          test.equal(value, 'A', 'a is A')
+          done() }) },
+      cache.put.bind(cache, 'c', 'C'),
+      function(done) {
+        cache.get('c', function(error, value) {
+          test.ifError(error, 'no error')
+          test.equal(value, 'C', 'c is C')
+          done() }) },
+      function(done) {
+        cache.get('b', function(error, value) {
+          test.ifError(error, 'no error')
+          test.equal(value, undefined, 'b is undefined')
+          done() }) },
+      function(done) {
+        cache.get('a', function(error, value) {
+          test.ifError(error, 'no error')
+          test.equal(value, 'A', 'a is A')
+          done() }) } ],
+    function(error) {
+      test.ifError(error, 'no error')
+      test.end() }) })
+
+tape.test('lru update via put', function(test) {
+  var cache = new testCache(2)
+  series(
+    [ cache.put.bind(cache, 'foo', '1'),
+      cache.put.bind(cache, 'bar', '2'),
+      cache.put.bind(cache, 'baz', '3'),
+      cache.put.bind(cache, 'qux', '4'),
+      function(done) {
+        cache.get('foo', function(error, value) {
+          test.ifError(error, 'no error')
+          test.equal(value, undefined, 'foo is undefined')
+          done() }) },
+      function(done) {
+        cache.get('bar', function(error, value) {
+          test.ifError(error, 'no error')
+          test.equal(value, undefined, 'bar is undefined')
+          done() }) },
+      function(done) {
+        cache.get('baz', function(error, value) {
+          test.ifError(error, 'no error')
+          test.equal(value, '3', 'baz is 3')
+          done() }) },
+      function(done) {
+        cache.get('qux', function(error, value) {
+          test.ifError(error, 'no error')
+          test.equal(value, '4', 'qux if 4')
+          done() }) } ],
+    function(error) {
+      test.ifError(error, 'no error')
+      test.end() }) })
